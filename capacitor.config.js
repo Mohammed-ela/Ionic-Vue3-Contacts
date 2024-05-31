50e87537c60e00cab8390753b2d427ce035b9caf
Clément Barbaza <clement.barbaza@gmail.com>
2024-05-31 23:40:19 +0200

    update capacitor

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,6 +1,4 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
+const config = {
   appId: 'io.ionic.starter',
   appName: 'teach-ionic8-vue3-contacts',
   webDir: 'dist'
