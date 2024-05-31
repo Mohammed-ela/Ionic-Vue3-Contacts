550c7b97979f04127922d7ac5cc1a287f36a69c3
Clément Barbaza <clement.barbaza@gmail.com>
2024-05-31 17:15:03 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'teach-ionic8-vue3-contacts',
+  webDir: 'dist'
+};
+
+export default config;
